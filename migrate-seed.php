d21028e16695cd5c058301358e09f61a9ae02bd6
Pedro Amaral <pedrosamaral@outlook.com>
2017-08-21 22:31:57 -0300

    New

--- migrate-seed.php
+++ migrate-seed.php
@@ -1,5 +1,6 @@
 <?php
 
-exec(__DIR__ . '/vendor/bin/phinx rollback -t=0');
+exec(__DIR__ . '/vendor/bin/phinx rollback -t 0');
 exec(__DIR__ . '/vendor/bin/phinx migrate');
-exec(__DIR__ . '/vendor/bin/phinx seed:run');
+exec(__DIR__ . '/vendor/bin/phinx seed:run -s UsersSeeder');
+exec(__DIR__ . '/vendor/bin/phinx seed:run -s CategoryCostsSeeder');
